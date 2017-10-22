3662f43fafaad0276563e1157508a67ace88138a
Pawel Stacherzak <staszko032@gmail.com>
2017-10-22 13:28:46 +0200

    Default test removed

--- src/test/java/com/staszkox/stacktrack/StacktrackApplicationTests.java
+++ src/test/java/com/staszkox/stacktrack/StacktrackApplicationTests.java
@@ -2,15 +2,13 @@ package com.staszkox.stacktrack;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class StacktrackApplicationTests {
-
+public class StacktrackApplicationTests 
+{
 	@Test
-	public void contextLoads() {
+	public void contextLoads() 
+	{
 	}
-
 }
